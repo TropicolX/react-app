5b911f9943292a2297a08af2a7991764b1acfb98
TropicolCoder <jacobsbusayo@gmail.com>
2021-06-12 17:46:46 +0100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// stateless functional component
+// always return JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX RULES
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
+/* const Greeting = () => {
+    return React.createElement(
+        'div',
+        {}, 
+        React.createElement('h1', {}, 'hello world')
+    );
+} */
+
+// Nested Components, React Tools
+
+function BookList() {
+    return (
+        <section>
+            <Book />
+        </section>
+    );
+}
+
+const Book = () => {
+    return (
+        <article>
+            this is a book
+        </article>
+    )
+}
+
+const Image = () => <img src="" alt="" />
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
